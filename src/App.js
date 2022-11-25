a1f1b83a24bb00800334c821dfe04e726bbf0c6e
Cristhian Benitez <cristhiantilleria@gmail.com>
2022-11-25 18:55:01 -0500

    setup context

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import React from 'react';
+import axios from 'axios';
+
+const api = 'https://restcountries.com/v3.1/region/asia';
+
+const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = React.useState(true);
+  const [score, setScore] = React.useState(0);
+  const [countries, setCountries] = React.useState([]);
+  const effectRan = React.useRef(false);
+
+  React.useEffect(() => {
+    if (effectRan.current) {
+      return;
+    }
+    if (effectRan.current === false) {
+      setLoading(true);
+
+      getCountries();
+      effectRan.current = true;
+    }
+  }, [countries]);
+
+  const getCountries = async () => {
+    const { data } = await axios(api);
+    const randomNumber = getRandomNumber(data.length);
+    const randomFourCountries = data.slice(randomNumber, randomNumber + 4);
+    setCountries(randomFourCountries);
+    setLoading(false);
+  };
+
+  const randomNumber = getRandomNumber(countries.length);
+  const selectedCountry = countries[randomNumber];
+
+  if (loading) return <div>loading</div>;
+  return <div className="country-quiz"></div>;
 }
 
 export default App;
